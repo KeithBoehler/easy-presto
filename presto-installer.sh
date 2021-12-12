a3b798e8ab9aed4b8ac01b7cff1412d1ab83189b
KeithBoehler <germanbohler@gmail.com>
2021-12-11 19:56:45 -0600

    Looks for source dir and makes it if not there

--- presto-installer.sh
+++ presto-installer.sh
@@ -1,9 +1,19 @@
 # A script to install presto and its dependencies 
 # Keith Boehler
 echo "Hello, lets get started!"
+echo "Current user: "  $(whoami)
+
+# To look for our install direcotry. Anything that gets downloaded manually or is done from
+# source will be put here. 
+ASTROSOFT=$HOME/ASTROSOFT
+if [ ! -d $ASTROSOFT ]; then
+	echo "Making astrosoft direcotry... "
+	mkdir $ASTROSOFT
+fi 
+
 ## Getting a few things 
-apt-get -qq update
-apt-get -qq --yes install csh gfortran autoconf meson libglib2.0-dev libcfitsio-bin libcfitsio-doc libcfitsio-dev libpng.dev zlib1g-dev libfftw3-bin libfftw3-dev
+sudo apt-get -qq update
+sudo apt-get -qq --yes install csh gfortran autoconf meson libglib2.0-dev libcfitsio-bin libcfitsio-doc libcfitsio-dev libpng.dev zlib1g-dev libfftw3-bin libfftw3-dev
 
 # Get anaconda
 
