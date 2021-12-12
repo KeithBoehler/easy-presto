c5c8101aa5169c90170212eab97afac56852f0e7
KeithBoehler <germanbohler@gmail.com>
2021-12-11 19:42:42 -0600

    Have apt-get install some needed packages ahead of time.

--- presto-installer.sh
+++ presto-installer.sh
@@ -3,7 +3,14 @@
 echo "Hello, lets get started!"
 ## Getting a few things 
 apt-get -qq update
-APTGET='-qq --yes --force-yes install'
-$APTGET csh
+apt-get -qq --yes install csh gfortran autoconf meson libglib2.0-dev libcfitsio-bin libcfitsio-doc libcfitsio-dev libpng.dev zlib1g-dev libfftw3-bin libfftw3-dev
+
+# Get anaconda
+
+# Get FFTW
+
+# Get PGPLOT
+
+# Get PRESTO
 
 
