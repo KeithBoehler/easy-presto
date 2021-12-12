fbd1581cb4e4c2a2d13665e6f89795695e06db9d
KeithBoehler <germanbohler@gmail.com>
2021-12-11 19:20:41 -0600

    Getting started. Just a hello world for now

---
+++ presto-installer.sh
@@ -0,0 +1,9 @@
+# A script to install presto and its dependencies 
+# Keith Boehler
+echo "Hello, lets get started!"
+## Getting a few things 
+apt-get -qq update
+APTGET='-qq --yes --force-yes install'
+$APTGET csh
+
+
