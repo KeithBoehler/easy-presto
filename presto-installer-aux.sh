b243875d17abecbf3e47bcb8ab94273382b628f1
KeithBoehler <germanbohler@gmail.com>
2021-12-19 18:19:30 -0600

    presto-installer-aux.sh is to try and get aroud the lack of new env vars when installing python. Might not need after all but i want to keep it handy

---
+++ presto-installer-aux.sh
@@ -0,0 +1,11 @@
+# Part 2 
+
+ASTROSOFT=$HOME/ASTROSOFT
+cd $ASTROSOFT
+echo 'hello again'
+echo $ASTROSOFT
+conda create --name pulsar python=3.7
+# I have had more success in installing with 3.7
+
+
+
